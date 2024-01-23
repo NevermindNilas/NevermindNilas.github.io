72cb0fe7d9d826284e429210a34bf2e960f4b7e5
NevermindNilas <nilascontact@gmail.com>
2024-01-23 03:40:36 +0200

    lowered intesnity

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ document.querySelectorAll('.hover3d').forEach(elem => {
         const yc = rect.height / 2;
         const dx = x - xc;
         const dy = y - yc;
-        this.style.transform = `perspective(500px) rotateX(${dy/-200}deg) rotateY(${dx/200}deg)`;
+        this.style.transform = `perspective(500px) rotateX(${dy/-500}deg) rotateY(${dx/500}deg)`;
     };
     elem.onmouseleave = function(e) {
         this.style.transform = `perspective(500px) rotateX(0) rotateY(0)`;
